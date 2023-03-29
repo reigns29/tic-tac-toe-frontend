607f6410e7848e95d36983a76cf0d5e60fe9eb74
DireWolf707 <rahulsood707@gmail.com>
2023-03-29 12:57:42 +0530

    added gamelist page

--- src/pages/GameList.jsx
+++ src/pages/GameList.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from "react"
+import { Stack, Typography } from "@mui/material"
+
+import TicTacToeCard from "../components/cards/TicTacToeCard"
 
 const GameList = () => {
   return (
-    <div>GameList</div>
+    <Stack sx={{ p: "32px", overflow: "auto" }} gap={2}>
+      <Typography variant="h4" fontWeight="bold" textAlign="center">
+        Game List
+      </Typography>
+      <Stack flexWrap="wrap" flexDirection="row" justifyContent="center" alignItems="center" gap={2}>
+        <TicTacToeCard />
+        <TicTacToeCard />
+        <TicTacToeCard />
+        <TicTacToeCard />
+        <TicTacToeCard />
+        <TicTacToeCard />
+      </Stack>
+    </Stack>
   )
 }
 
