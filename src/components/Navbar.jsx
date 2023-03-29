3fd489bb96f3075e5dd3562af84a3d5e9aeb2037
DireWolf707 <rahulsood707@gmail.com>
2023-03-29 12:58:05 +0530

    added link to game list in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,16 @@
 import React from "react"
 import { Stack, Typography } from "@mui/material"
 import { ConnectButton } from "@web3uikit/web3"
+import { Link } from "react-router-dom"
 
 const Navbar = () => {
   return (
     <Stack flexDirection="row" justifyContent="space-between" alignItems="center" sx={{ p: "16px", borderBottom: "4px solid red" }}>
-      <Typography variant="h5" color="red" fontWeight="bold">
-        Blockchain Gamerz
-      </Typography>
+      <Link to="/games">
+        <Typography variant="h5" color="red" fontWeight="bold">
+          Blockchain Gamerz
+        </Typography>
+      </Link>
       <ConnectButton moralisAuth={false} />
     </Stack>
   )
